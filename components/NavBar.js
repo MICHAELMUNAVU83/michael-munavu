fb4c8b5697147a63019b9d0e70359be4a81137ea
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-27 15:57:57 +0300

    Added react icons

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,41 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const NavBar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-15">
+        <Image src="/../public/assets/navLogo.png" width={125} height={50} />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 uppercase text-sm hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 uppercase text-sm hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 uppercase text-sm hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 uppercase text-sm hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 uppercase text-sm hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
