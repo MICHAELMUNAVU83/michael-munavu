c8bf799465ffcc899d6aa719c51bddd0a0063f98
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-28 23:26:16 +0300

    Polished about

--- components/About.js
+++ components/About.js
@@ -4,7 +4,10 @@ import avatar from "../public/avatar.png";
 import Link from "next/link";
 const About = () => {
   return (
-    <div id="about" className="w-full h-screen md:h-screen p-2 flex items-center ">
+    <div
+      id="about"
+      className="w-full h-screen md:h-screen p-2 flex items-center "
+    >
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xltracking-widest text-[#5651e5] ">
@@ -12,16 +15,59 @@ const About = () => {
           </p>
           <h2 className="py-4">Who am I?</h2>
           <p className="py-2 text-gray-600">
-            I am not a designer, but I design. I am not a developer, but I
-            develop. I am not a writer, but I write.My name is Michael Munavu, a
+            I am a third year Telecommunications Engineering student at JKUAT
+            who was first a{" "}
+            <span className=" text-[#5651e5]">problem solver</span> and later
+            became
+            <span className=" text-[#5651e5]">
+              {" "}
+              a full stack software engineer .
+            </span>{" "}
+            and
+            <span className=" text-[#5651e5]">Technical Writer</span>
           </p>
           <p className="py-2 text-gray-600">
-            I am not a designer, but I design. I am not a developer, but I
-            develop. I am not a writer, but I write.My name is Michael Munavu, a
+            I have experienced working in technology for the{" "}
+            <span className=" text-[#5651e5]">past 3 years</span> and a graduant
+            from <span className=" text-[#5651e5]">Microverse,</span> a remote
+            International learning institution where I learned through pair
+            programming and project building. We worked on many different
+            technologies like{" "}
+            <span className=" text-[#5651e5]">
+              HTML, CSS, JS, React, Postgresql, Ruby on Rails,
+            </span>{" "}
+            and collaborated with developers across the globe . I have also
+            completed a software development program at
+            <span className=" text-[#5651e5]">Moringa School</span>
+            which is the top programming school in Kenya.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            I have been part of a team that won two{" "}
+            <span className=" text-[#5651e5]">Hackathons in the past year</span>
+            I was the lead backend engineer for both projects where we built a
+            <span className=" text-[#5651e5]">
+              system to help patients store and access their medical records
+              efficiently
+            </span>
+          </p>
+          <p className="py-2 text-gray-600">
+            I noticed I love teaching and helping people learn new things. I
+            <span className=" text-[#5651e5]">write technical articles</span> on
+            React and Ruby on Rails mostly on
+            <span className=" text-[#5651e5]">
+              <a
+                href="https://medium.com/@michaelmunavu83"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Medium
+              </a>
+            </span>
           </p>
           <p className="py-2 text-gray-600 underline cursor-pointer">
             <Link href="/#projects">
-            Check out some of my recent projects below.
+              Check out some of my recent projects below.
             </Link>
           </p>
         </div>
