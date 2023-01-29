1103959da156f489862649af4e4a5610f2b7d2e9
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-29 20:56:19 +0300

    Added framer motion

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,85 +2,93 @@ import React from "react";
 import { FaLinkedin, FaGithub, FaTwitter, FaWhatsapp } from "react-icons/fa";
 import { FiPhoneCall } from "react-icons/fi";
 import Typewriter from "typewriter-effect";
+import { motion } from "framer-motion";
 const Main = () => {
   return (
-    <div id="home" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] mx-auto h-full flex p-2 justify-center items-center">
-        <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600 ">
-            LET'S BUILD SOMETHING BEAUTIFUL
-          </p>
-          <h1 className="py-3 text-gray-700">
-            Hi , I am <span className="text-[#5651e5]">Michael Munavu</span>
-          </h1>
-          <h2 className=" text-gray-700 text-center">I am a</h2>
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 30 }}
+      transition={{ duration: 2 }}
+    >
+      <div id="home" className="w-full h-screen text-center">
+        <div className="max-w-[1240px] mx-auto h-full flex p-2 justify-center items-center">
+          <div>
+            <p className="uppercase text-sm tracking-widest text-gray-600 ">
+              LET'S BUILD SOMETHING BEAUTIFUL
+            </p>
+            <h1 className="py-3 text-gray-700">
+              Hi , I am <span className="text-[#5651e5]">Michael Munavu</span>
+            </h1>
+            <h2 className=" text-gray-700 text-center">I am a</h2>
 
-          <h1 className="py-3 text-[#5651e5]">
-            <Typewriter
-              options={{
-                strings: [
-                  "Full Stack Developer",
-                  "React Developer",
-                  "Ruby on Rails Developer",
-                  "Technical Writer",
-                ],
-                autoStart: true,
-                loop: true,
-                cursor: "",
-              }}
-            />
-          </h1>
-          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-            I am a full-stack developer with 3 years of experience having been
-            self-taught and learning from Microverse and Moringa Bootcamps both
-            remotely.
-          </p>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer  hover:scale-110 ease-in   duration-300">
-              <a
-                href="https://www.linkedin.com/in/michael-munavu-0b0b1b1b9/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaLinkedin />
-              </a>
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in   duration-300">
-              <a
-                href="https://github.com/MICHAELMUNAVU83"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaGithub />
-              </a>
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in   duration-300">
-              <a
-                href="https://twitter.com/MichaelTrance1"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaTwitter />
-              </a>
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in   duration-300">
-              <a rel="noopener noreferrer" href="tel:0740769596">
-                <FiPhoneCall />
-              </a>
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in   duration-300">
-              <a
-                href="https://wa.me/254740769596"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaWhatsapp />
-              </a>
+            <h1 className="py-3 text-[#5651e5]">
+              <Typewriter
+                options={{
+                  strings: [
+                    "Full Stack Developer",
+                    "React Developer",
+                    "Ruby on Rails Developer",
+                    "Technical Writer",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                  cursor: "",
+                }}
+              />
+            </h1>
+            <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+              I am a full-stack developer with 3 years of experience having been
+              self-taught and learning from Microverse and Moringa Bootcamps
+              both remotely.
+            </p>
+            <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer  hover:scale-110 ease-in   duration-300">
+                <a
+                  href="https://www.linkedin.com/in/michael-munavu-0b0b1b1b9/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaLinkedin />
+                </a>
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in   duration-300">
+                <a
+                  href="https://github.com/MICHAELMUNAVU83"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub />
+                </a>
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in   duration-300">
+                <a
+                  href="https://twitter.com/MichaelTrance1"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaTwitter />
+                </a>
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in   duration-300">
+                <a rel="noopener noreferrer" href="tel:0740769596">
+                  <FiPhoneCall />
+                </a>
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in   duration-300">
+                <a
+                  href="https://wa.me/254740769596"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaWhatsapp />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- components/Skills.js
+++ components/Skills.js
@@ -8,105 +8,108 @@ import tailwind from "../public/assets/skills/tailwind.png";
 import nextjs from "../public/assets/skills/nextjs.png";
 import ruby from "../public/assets/skills/ruby.png";
 import rails from "../public/assets/skills/ruby-on-rails.png";
+import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
-    <div id="skills" className="w-full  p-2 mt-2">
-      <div className="max-w-[1240px] m-auto flex  flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5] ">
-          Skills
-        </p>
-        <h2 className="py-4">What I can do</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4  justify-center items-center">
-              <div className="m-auto">
-                <Image src={html} alt="html" width={50} height={50} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <div id="skills" className="w-full  p-2 mt-2">
+        <div className="max-w-[1240px] m-auto flex  flex-col justify-center h-full">
+          <p className="text-xl tracking-widest uppercase text-[#5651e5] ">
+            Skills
+          </p>
+          <h2 className="py-4">What I can do</h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4  justify-center items-center">
+                <div className="m-auto">
+                  <Image src={html} alt="html" width={50} height={50} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>HTML</h3>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4  justify-center items-center">
-              <div className="m-auto">
-                <Image src={css} alt="html" width={50} height={50} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>CSS</h3>
+            <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4  justify-center items-center">
+                <div className="m-auto">
+                  <Image src={css} alt="html" width={50} height={50} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>CSS</h3>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4  justify-center items-center">
-              <div className="m-auto">
-                <Image src={javascript} alt="html" width={50} height={50} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Javascript</h3>
+            <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4  justify-center items-center">
+                <div className="m-auto">
+                  <Image src={javascript} alt="html" width={50} height={50} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Javascript</h3>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4  justify-center items-center">
-              <div className="m-auto">
-                <Image src={react} alt="html" width={50} height={50} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>REACT JS</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4  justify-center items-center">
-              <div className="m-auto">
-                <Image src={ruby} alt="html" width={50} height={50} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>RUBY</h3>
+            <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4  justify-center items-center">
+                <div className="m-auto">
+                  <Image src={react} alt="html" width={50} height={50} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>REACT JS</h3>
+                </div>
               </div>
             </div>
-          </div>
+            <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4  justify-center items-center">
+                <div className="m-auto">
+                  <Image src={ruby} alt="html" width={50} height={50} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>RUBY</h3>
+                </div>
+              </div>
+            </div>
 
-          <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4  justify-center items-center">
-              <div className="m-auto">
-                <Image src={rails} alt="html" width={50} height={50} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>RUBY ON RAILS</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4  justify-center items-center">
-              <div className="m-auto">
-                <Image src={nextjs} alt="html" width={50} height={50} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>NEXT JS</h3>
+            <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4  justify-center items-center">
+                <div className="m-auto">
+                  <Image src={rails} alt="html" width={50} height={50} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>RUBY ON RAILS</h3>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4  justify-center items-center">
-              <div className="m-auto">
-                <Image src={tailwind} alt="html" width={50} height={50} />
+            <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4  justify-center items-center">
+                <div className="m-auto">
+                  <Image src={nextjs} alt="html" width={50} height={50} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>NEXT JS</h3>
+                </div>
               </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>TAILWIND CSS</h3>
+            </div>
+
+            <div className="p-6 shadow-xl rounded-xl  hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4  justify-center items-center">
+                <div className="m-auto">
+                  <Image src={tailwind} alt="html" width={50} height={50} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>TAILWIND CSS</h3>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
