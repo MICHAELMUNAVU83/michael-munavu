64591fbd20957921f982b492d073f7a72133044b
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-29 08:29:41 +0300

    Changed width

--- components/BlogItem.js
+++ components/BlogItem.js
@@ -6,7 +6,7 @@ const BlogItem = ({ title, image, projectUrl, languages }) => {
     <div class="flex justify-center shadow-2xl shadow-gray-500 hover:scale-105 transform transition duration-500 ease-in-out">
       <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
         <Image
-          class=" w-full h-50 md:h-50 object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+          class=" w-75 h-50 md:h-50 object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
           src={image}
           alt=""
         />
