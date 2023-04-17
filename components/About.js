26aaf91b73d46352685a74b66c926650cfac7ee0
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-17 19:05:58 +0300

    Changed space

--- components/About.js
+++ components/About.js
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <div
       id="about"
-      className="w-full h-full md:h-screen p-2 flex items-center "
+      className="w-full my-8 md:my--0 h-full md:h-screen p-2 flex items-center "
     >
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2 p-2">
